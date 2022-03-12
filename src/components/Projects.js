c3a3457c14ff0981ae9032d0a423ab0b71e041c4
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2022-03-12 18:03:22 +0100

    projects edited

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,29 +4,25 @@ const projects = [{
   title: 'OptiWeather',
   description: 'A simple weather app built with Reactjs and SCSS which fetches weather info of given locations and renders them as cards.',
   project_url: 'https://optix-weather-app.netlify.app/',
-  repo: 'https://github.com/OpTiX6696/Weather-App',
-  bg: ''
+  repo: 'https://github.com/OpTiX6696/Weather-App'
 },
 {
-  title: 'OptiWeather',
-  description: 'A simple weather app built with Reactjs and SCSS which fetches weather info of given locations and renders them as cards.',
-  project_url: 'https://optix-weather-app.netlify.app/',
-  repo: 'https://github.com/OpTiX6696/Weather-App',
-  bg: ''
+  title: 'Sea Water Level Predictor',
+  description: 'A python code that uses sea water level data from previous years to predict future sea water level and returns a graph of year against sea water level.',
+  project_url: '#',
+  repo: 'https://github.com/OpTiX6696/Sea_Level_Predictor'
 },
 {
-  title: 'OptiWeather',
-  description: 'A simple weather app built with Reactjs and SCSS which fetches weather info of given locations and renders them as cards.',
-  project_url: 'https://optix-weather-app.netlify.app/',
-  repo: 'https://github.com/OpTiX6696/Weather-App',
-  bg: ''
+  title: 'QUIZAPP',
+  description: 'A quiz app that lets players choose from different sections. Players are ranked on a leaderboard, based on their scores.',
+  project_url: '#',
+  repo: 'https://github.com/OpTiX6696/QuizApp'
 }
 // {
 //   title: '',
 //   description: '',
 //   project_url: '',
-//   repo: '',
-//   bg: ''
+//   repo: ''
 // }
 
 ]
