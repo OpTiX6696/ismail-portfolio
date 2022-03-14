7ea12a78f1521da23dc5c5cb42d7cd561f54600b
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2022-03-14 21:18:17 +0100

    header background fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import '../styles/Header.scss';
 // import Navbar from './Navbar';
 import Logo from './Logo'
@@ -8,8 +8,23 @@ import Hamburger from './Hamburger';
 
 const Header = () => {
 
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = (e) => {
+    if (e.currentTarget.scrollY > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false)
+    }
+    console.log(e.currentTarget.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  })
+
   return (
-    <div className='header'>
+    <div className= {`header ${scrolled ? 'scrolled' : ''}`}>
       <Logo />
       {/* <Navbar /> */}
       <Hamburger />
